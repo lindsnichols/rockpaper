0b86198e8c72cf7eabf5acd8b88e4dd9faa00017
Lindsay <lindsayanichols22@gmail.com>
2023-10-16 12:17:28 -0700

    add UI

--- logic.js
+++ logic.js
@@ -1,51 +1,72 @@
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+const buttonArea = document.querySelector(".buttons");
+const textArea = document.querySelector(".info");
+const scoreArea = document.querySelector(".score");
+const bigscoreArea = document.querySelector(".bigscore");
+
+const WIN = 1;
+const TIE = 0;
+const LOSE = -1;
+
+let playerWins = 0;
+let computerWins = 0;
+
 function getComputerChoice() {
   let array = ["rock", "paper", "scissors"];
-  return array[Math.floor(Math.random() * array.length)];
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 }
 
 function singleRound(computerChoice, playerChoice) {
-  playerChoice.toLowerCase();
-
   console.log(`Computer plays ${computerChoice}`);
   console.log(`Player plays ${playerChoice}`);
 
   if (computerChoice == playerChoice) {
-    console.log("TIE!");
-    return 0;
+    textArea.textContent = "TIE!";
+    return TIE;
   }
   if (
     (computerChoice == "rock" && playerChoice == "paper") ||
     (computerChoice == "paper" && playerChoice == "scissors") ||
     (computerChoice == "scissors" && playerChoice == "rock")
   ) {
-    console.log("YOU WIN!");
-    return 1;
+    textArea.textContent = "YOU WIN!";
+    return WIN;
   } else {
-    console.log("you lose.");
-
-    return -1;
+    textArea.textContent = "YOU LOSE!";
+    return LOSE;
   }
 }
 
-function game() {
-  let wins = 0;
-  for (let i = 1; i <= 5; i++) {
-    let currentRound = 0;
-    while (currentRound == 0) {
-      let playerChoice = prompt(`Round ${i}:`);
-      currentRound = singleRound(getComputerChoice(), playerChoice);
-    }
-    wins += currentRound;
+buttonArea.addEventListener("click", (event) => {
+  bigscoreArea.textContent = "";
+  let choice = event.target.id;
+  let outcome = singleRound(getComputerChoice(), choice);
+  updateScore(outcome);
+});
+
+function updateScore(outcome) {
+  if (outcome == WIN) {
+    playerWins += 1;
   }
-  if (wins == 0) {
-    console.log("TIE OVERALL!");
+  if (outcome == LOSE) {
+    computerWins += 1;
   }
-  if (wins > 0) {
-    console.log("YOU WIN OVERALL!");
+
+  scoreArea.textContent = `Computer: ${computerWins}, Player: ${playerWins}`;
+  if (playerWins == 5) {
+    bigscoreArea.textContent = "YOU WIN OVERALL";
+    restartGame();
   }
-  if (wins < 0) {
-    console.log("YOU LOSE OVERALL!");
+  if (computerWins == 5) {
+    bigscoreArea.textContent = "YOU LOSE OVERALL";
+    restartGame();
   }
 }
 
-game();
+function restartGame() {
+  playerWins = 0;
+  computerWins = 0;
+}
